ca2881299df73336ae959f5bc4d19853769b51fc
NavishaShetty <you@example.com>
2025-07-27 09:12:15 +1000

    setting up environment to install kubespray

---
+++ 02_env_prep.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e  # Exit on any error
+
+# Configuration variables
+#KUBESPRAY_VERSION="v2.24.0"  # Change to desired version
+CLUSTER_NAME="k8s-cluster"
+#PYTHON_VERSION="3.9"
+
+# Function definitions (you'll need to add these)
+log() {
+    echo "[INFO] $1"
+}
+
+warn() {
+    echo "[WARN] $1"
+}
+
+# Install required tools locally with non-interactive flags
+export DEBIAN_FRONTEND=noninteractive
+apt update -y
+apt install -y sudo
+sudo apt update -y
+
+# Add the official Ansible PPA non-interactively
+apt-get update -y
+apt-get install -y software-properties-common
+add-apt-repository --yes --update ppa:ansible/ansible
+apt-get install -y ansible
+
+log "Starting Kubespray installation process..."
+
+# If Kubespray already exists, remove it
+if [ -d "kubespray" ]; then
+    warn "kubespray directory already exists, removing it..."
+    rm -rf kubespray
+fi
+
+# Clone Kubespray
+git clone https://github.com/kubernetes-sigs/kubespray.git
+cd kubespray
+
+# Install dependencies
+sudo pip3 install -r requirements.txt
+
+# 5. Copy sample inventory
+log "Setting up inventory..."
+cp -rfp inventory/sample inventory/${CLUSTER_NAME}
